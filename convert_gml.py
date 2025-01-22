057eff612e265b796212cf9d841e22207c213bf0
Lyam Chardey <lyam.chardey@insa-rouen.fr>
2025-01-22 14:49:26 +0100

    Updated functions to also work with gml strings (as gml file content)

--- convert_gml.py
+++ convert_gml.py
@@ -4,12 +4,14 @@ Conversion for molecules graphs between `torch_geometric.data.Data` and `gml` fi
 
 import os
 import sys
+import io
+from typing import Optional
 import zipfile
 import networkx as nx
 import torch
 from torch_geometric.data import Data
 
-def data_to_gml(data: Data, output: str) -> None:
+def data_to_gml(data: Data, output: Optional[str] = None) -> Optional[str]:
     """
     Writes a `Data` object into a `gml` file
 
@@ -17,7 +19,14 @@ def data_to_gml(data: Data, output: str) -> None:
     ------------
 
     * `data`   : `Data` object to save
-    * `output` : output file path
+    * `output` : output file path (Default value `None`).
+        If `None` is given, will return the file content instead
+
+    Returns :
+    ---------
+
+    * `str`  : The gml file content if no output file was given
+    * `None` : If an output file was given
     """
     g = nx.Graph()
 
@@ -32,23 +41,45 @@ def data_to_gml(data: Data, output: str) -> None:
         )
 
     g.graph["y"] = data.y.item()
+
+    if output is None:
+        output = io.StringIO()
     nx.write_gml(g, output)
 
-def gml_to_data(gml: str) -> Data:
+    if isinstance(output, io.StringIO):
+        result = output.getvalue()
+        output.close()
+        return result
+
+def gml_to_data(gml: str, gml_file: bool = True) -> Data:
     """
     Reads a `gml` file and creates a `Data` object
 
     Parameters :
     ------------
 
-    * `gml` : gml file path
+    * `gml`      : gml file path if `gml_file` is set to `True`, gml content otherwise
+    * `gml_file` : indicates whether `gml` is a path to a gml file or a gml file content
 
     Returns :
     ---------
 
     * `Data` : the `Data` object created from the file content
+
+    Raises :
+    --------
+
+    * `FileNotFoundError` : if `gml` is a file path and doesn't exist
     """
-    g: nx.Graph = nx.read_gml(gml)
+    if gml_file:
+        if not os.path.exists(gml):
+            raise FileNotFoundError(f"File `{gml}` does not exist")
+        g: nx.Graph = nx.read_gml(gml)
+    else:
+        buffer = io.StringIO(gml)
+        g: nx.Graph = nx.read_gml(buffer)
+        buffer.close()
+
     x, edge_index, edge_attr = [], [], []
 
     y = torch.tensor([g.graph["y"]], dtype=torch.long) if "y" in g.graph else None
